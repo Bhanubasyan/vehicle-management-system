2f453906153eb1a1f1e6258f0c942506fa287c0d
bhanu1012 <bhanubasyan@gmail.com>
2026-03-18 22:15:51 +0530

    deploy backend on render

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -27,7 +27,7 @@ function Dashboard() {
 const [showSearch, setShowSearch] = useState(false);
 const [showFilter, setShowFilter] = useState(false);
   useEffect(() => {
-    fetch("http://localhost:5000/vehicles")
+    fetch("https://vehicle-management-system-5d2c.onrender.com//vehicles")
       .then(res => res.json())
       .then(data => setVehicles(data));
   }, []);
